d1cc2dc0a78758be74fcca16f9700745b2cadd88
Kara Underwood <karaunderwood@Karas-MacBook-Pro.local>
2020-12-16 11:20:00 -0600

    on to step 28.7.2.2

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Cool Pics';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://s7.orientaltrading.com/is/image/OrientalTrading/VIEWER_ZOOM/buddy-the-elf-outdoor-stand-up~13808695';
+  image3 = 'https://www.theribboninmyjournal.com/wp-content/uploads/2018/12/sisters-758x569.jpg';
 
   constructor() { }
 
